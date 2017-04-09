f1bc5e760728a9fe7f541560235318e0e64802e2
JVM <malajv01@students.ipfw.edu>
2017-04-09 15:10:27 -0400

    Did some work on the main form.

--- CSharpFinal/Form1.Designer.cs
+++ CSharpFinal/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSharpFinal
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.characterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.grpSearch = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radCharacter = new System.Windows.Forms.RadioButton();
+            this.radItem = new System.Windows.Forms.RadioButton();
+            this.btnSearchLike = new System.Windows.Forms.Button();
+            this.btnSearchExact = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.grpSearch.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem,
+            this.characterToolStripMenuItem,
+            this.itemsToolStripMenuItem,
+            this.resetToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(508, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.resetToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            // 
+            // characterToolStripMenuItem
+            // 
+            this.characterToolStripMenuItem.Name = "characterToolStripMenuItem";
+            this.characterToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.characterToolStripMenuItem.Text = "&Character";
+            // 
+            // itemsToolStripMenuItem
+            // 
+            this.itemsToolStripMenuItem.Name = "itemsToolStripMenuItem";
+            this.itemsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.itemsToolStripMenuItem.Text = "&Items";
+            // 
+            // resetToolStripMenuItem1
+            // 
+            this.resetToolStripMenuItem1.Name = "resetToolStripMenuItem1";
+            this.resetToolStripMenuItem1.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem1.Text = "&Reset";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(484, 335);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // grpSearch
+            // 
+            this.grpSearch.Controls.Add(this.btnSearchExact);
+            this.grpSearch.Controls.Add(this.btnSearchLike);
+            this.grpSearch.Controls.Add(this.radItem);
+            this.grpSearch.Controls.Add(this.radCharacter);
+            this.grpSearch.Controls.Add(this.textBox1);
+            this.grpSearch.Location = new System.Drawing.Point(13, 28);
+            this.grpSearch.Name = "grpSearch";
+            this.grpSearch.Size = new System.Drawing.Size(483, 66);
+            this.grpSearch.TabIndex = 5;
+            this.grpSearch.TabStop = false;
+            this.grpSearch.Text = "Quick Search";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // radCharacter
+            // 
+            this.radCharacter.AutoSize = true;
+            this.radCharacter.Location = new System.Drawing.Point(169, 19);
+            this.radCharacter.Name = "radCharacter";
+            this.radCharacter.Size = new System.Drawing.Size(71, 17);
+            this.radCharacter.TabIndex = 1;
+            this.radCharacter.TabStop = true;
+            this.radCharacter.Text = "Character";
+            this.radCharacter.UseVisualStyleBackColor = true;
+            // 
+            // radItem
+            // 
+            this.radItem.AutoSize = true;
+            this.radItem.Location = new System.Drawing.Point(169, 42);
+            this.radItem.Name = "radItem";
+            this.radItem.Size = new System.Drawing.Size(45, 17);
+            this.radItem.TabIndex = 2;
+            this.radItem.TabStop = true;
+            this.radItem.Text = "Item";
+            this.radItem.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchLike
+            // 
+            this.btnSearchLike.Location = new System.Drawing.Point(278, 20);
+            this.btnSearchLike.Name = "btnSearchLike";
+            this.btnSearchLike.Size = new System.Drawing.Size(101, 40);
+            this.btnSearchLike.TabIndex = 3;
+            this.btnSearchLike.Text = "Search";
+            this.btnSearchLike.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchExact
+            // 
+            this.btnSearchExact.Location = new System.Drawing.Point(385, 19);
+            this.btnSearchExact.Name = "btnSearchExact";
+            this.btnSearchExact.Size = new System.Drawing.Size(92, 40);
+            this.btnSearchExact.TabIndex = 4;
+            this.btnSearchExact.Text = "Search Exact";
+            this.btnSearchExact.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 420);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(508, 447);
+            this.Controls.Add(this.grpSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.grpSearch.ResumeLayout(false);
+            this.grpSearch.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem characterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox grpSearch;
+        private System.Windows.Forms.Button btnSearchExact;
+        private System.Windows.Forms.Button btnSearchLike;
+        private System.Windows.Forms.RadioButton radItem;
+        private System.Windows.Forms.RadioButton radCharacter;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
